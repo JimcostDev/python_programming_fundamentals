8489081470cf9b8a4ce71c7a6e446225d3e564d2
JimcostDev <rjak727@gmail.com>
2023-06-09 01:40:37 +0200

    fundamentos python

---
+++ fundamentos_python/01_variables.py
@@ -0,0 +1,12 @@
+
+"""
+ este es un programa para explicar el concepto de variables en python 
+ autor: Ronaldo Jimenez
+ web: www.jimcostdev.com
+"""
+# variable = es un espacio en memoria que su valor cambia a lo largo del algoritmo
+
+variable = True
+
+# imprimir el valor de mi variable
+print(variable)
---
+++ fundamentos_python/02_tipos_datos.py
@@ -0,0 +1,18 @@
+# algoritmo que nos muestra que tipo de dato captura una variable
+
+# variable para almacenar diferetes tipos de datos
+variable = 8
+tipo_dato = type(variable).__name__
+
+if tipo_dato == 'int':
+    print('la variable es un número de tipo: entero')
+elif tipo_dato == 'float':
+    print('la variable es un número de tipo: float')
+elif tipo_dato == 'str':
+    print('la variable es de tipo: cadena')
+elif tipo_dato == 'bool':
+    print('la variable es de tipo: booleana')
+elif tipo_dato == 'list':
+    print('la variable es de tipo: lista')
+elif tipo_dato == 'dict':
+    print('la variable es de tipo: diccionario')
