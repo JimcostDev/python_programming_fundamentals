511ee88adeaf5255a040717eba8b453c3435c9c7
JimcostDev <rjak727@gmail.com>
2023-06-12 00:59:28 +0200

    tipos de datos: numeros

---
+++ fundamentos_python/04_numero.py
@@ -0,0 +1,30 @@
+# tipos de numeros
+edad = 23
+print(type(edad))
+print(edad)
+
+numero_1 = 2
+numero_2 = 4
+
+resultado = numero_1 / numero_2
+print(type(resultado))
+print(resultado)
+
+vidas = 3
+vidas -= 1 # vidas = vidas - 1
+print(vidas)
+
+vidas += 5 # vidas = vidas + 5
+print(vidas)
+
+
+# formato de numero cientifico
+numero_1 = 4500000000000000000.1
+print(numero_1)
+numero_2 = 0.0000000001
+print(numero_2)
+
+# leer datos
+edad = input('Por favor dime tu edad: ')
+print(type(edad))
+print(edad)
