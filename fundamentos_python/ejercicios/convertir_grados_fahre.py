e1e0f2ce03af55a5042fecc5a9c9970fb12c997a
JimcostDev <rjak727@gmail.com>
2023-06-12 13:16:42 +0200

    grados celsius a fahrenheit

---
+++ fundamentos_python/ejercicios/convertir_grados_fahre.py
@@ -0,0 +1,12 @@
+# Declaración de variables
+celsius = 0.0  
+fahrenheit  = 0.0
+
+# Entrada de datos
+celsius = float(input('Ingrse el valor de los grados en Celsius: '))
+
+# Conversión de grados Celsius a Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Salida de resultados
+print(f"La temperatura en grados Fahrenheit es: {fahrenheit}") 
