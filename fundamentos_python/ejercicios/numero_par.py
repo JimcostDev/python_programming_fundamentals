019f57c394b16ff294d4f996235efb6be49fdde2
JimcostDev <rjak727@gmail.com>
2023-06-15 16:08:47 +0200

    determinar si un número es par o impar

---
+++ fundamentos_python/ejercicios/numero_par.py
@@ -0,0 +1,11 @@
+# condicionales
+numero = 9
+if (numero % 2 == 0):
+    print('Es par')
+else:
+    print('Es impar')
+
+# uso de listas
+resultado = ["Par", "Impar"]
+resultado = resultado[numero % 2]
+print(resultado)
