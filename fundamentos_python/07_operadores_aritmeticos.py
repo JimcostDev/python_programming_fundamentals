f9540a5c6b846cc681cd7e15f42d8f3fc6cc2232
JimcostDev <rjak727@gmail.com>
2023-06-16 00:25:52 +0200

    operadores aritmeticos

---
+++ fundamentos_python/07_operadores_aritmeticos.py
@@ -0,0 +1,36 @@
+# operaciones basicas (suma, resta, multiplicacion y division)
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5.0
+print(10 // 2) # 5
+print("_" * 10)
+
+# Modulo
+print(10 % 2) # 0
+print(10 / 3)
+print(10 % 3)
+print("_" * 10)
+
+# exponenciacion 
+print(2 ** 3)
+
+# jerarquia de operadores
+print(2 ** 3 + 3 - 7 / 1 // 4)
+"""
+P = paréntesis ()
+E = exponentes
+M = multiplicación
+D = división
+A = adicción
+S = sustración
+"""
+
+print(2 ** 3)
+print((7 / 1) // 4)
+
+print(8 + 3 - 1)
+#print(10 / 0)
+
+print('Hola' + ' mundo')
+print('Hola' * 20)
