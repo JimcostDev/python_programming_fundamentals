b2f34dee5dc5f8737e8a747429a6a24795fb485b
JimcostDev <rjak727@gmail.com>
2023-06-16 00:36:12 +0200

    operadores relacionales

---
+++ fundamentos_python/08_operadores_de_comparacion.py
@@ -0,0 +1,43 @@
+# > 
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+print("_" * 10)
+
+# <
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+print("_" * 10)
+
+# >=
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+print("_" * 10)
+
+# <=
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+print("_" * 10)
+
+# ==
+print(6 == 6)
+print(5 == 2)
+print("_" * 10)
+
+# !=
+print(6 != 10)
+print(6 != 6)
+print("_" * 10)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1)
+print("_" * 10)
+
+edad = 18
+print(edad >= 18)
+
+# # lectura: https://www.freecodecamp.org/espanol/news/operadores-basicos-en-python-con-ejemplos/
