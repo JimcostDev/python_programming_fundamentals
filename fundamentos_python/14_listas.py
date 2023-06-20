cbbb3c2179253bb2e29de750dbbbd8fed59d9b3a
JimcostDev <rjak727@gmail.com>
2023-06-21 00:19:43 +0200

    listas

---
+++ fundamentos_python/14_listas.py
@@ -0,0 +1,24 @@
+numeros = [1, 2, 3, 4]
+print(numeros)
+print(type(numeros))
+
+tareas = ['limpiar la casa', 'ver el partido']
+print(tareas)
+
+tipos = [1, True, 'hola']
+print(tipos)
+
+print(numeros[0])
+print(tareas[1])
+text = 'Hola'
+# text[0] = 'W'
+
+tareas[0] = 'ir a la playa'
+print(tareas)
+
+tareas[0] = 'lavar los platos'
+print(tareas)
+
+print(numeros[:3])
+print(True in tipos)
+print('hola' in tipos)
