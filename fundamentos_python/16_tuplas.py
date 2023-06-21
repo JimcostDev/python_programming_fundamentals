5622564167a3cd78ba8272fdc83d4837dcf43096
JimcostDev <rjak727@gmail.com>
2023-06-21 12:43:03 +0200

    tuplas

---
+++ fundamentos_python/16_tuplas.py
@@ -0,0 +1,39 @@
+
+# Las tuplas son una estructura de datos inmutable que contiene una secuencia ordenada de elementos
+# Tupla = (1, 2, 3, 4)
+
+# Los elementos están separados por espacios luego de las comas
+# Puede contener cualquier tipo de datos
+# Cada posición de la tupla tiene un índice
+# Podriamos decir que son solo de lectura
+
+numeros = (1, 2, 3, 5)
+strings = ('ronny', 'jimcostdev', 'clau', 'marco', 'jimcostdev')
+print(numeros)
+print('0 =>', numeros[0])
+print('-1 =>', numeros[-1])
+print(type(numeros))
+
+print(strings)
+print(type(strings))
+
+# CRUD
+# numeros.append(10)
+print(numeros)
+# numeros[1] = 'change'
+
+print(strings)
+print(strings.index('marco'))
+print(strings.count('jimcostdev'))
+
+# convertir tupla a lista
+lista = list(strings)
+print(lista)
+print(type(lista))
+
+lista[1] = 'juli'
+print(lista)
+
+# convertir lista a tupla
+tupla = tuple(lista)
+print(tupla)
