be94e4168a071aae130d2cff1f70cd9bfc18b286
JimcostDev <rjak727@gmail.com>
2023-06-22 14:09:02 +0200

    ciclo while

---
+++ fundamentos_python/18_while.py
@@ -0,0 +1,17 @@
+'''
+while True:
+    print('Mientras la condicon sea "True" el bloque de codigo se va a ejecutar')
+    # esto es un ciclo infinito porque siempre esta en True
+'''
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+
+# ejemplo practico
+numero_secreto = 10
+print('------ ADIVINA EL NÚMERO -----')
+while True:
+    adivina = int(input('Escribe el número => '))
+    if adivina == numero_secreto:
+        break # romper la ejecución del ciclo (stop)
