2439667f450ad9731bd9f8859a4db779daef99cd
JimcostDev <rjak727@gmail.com>
2024-04-25 19:26:28 +0200

    add docstring

---
+++ 01_python_basico/docstring.py
@@ -0,0 +1,12 @@
+def greet(name):
+  """This function greets the user by name.
+
+  Args:
+      name: The name of the user to greet (str).
+
+  Returns:
+      A string containing the greeting message (str).
+  """
+  return f"Hello, {name}!"
+
+print(greet.__doc__)
